6b78df5218a23357ff2ecacf66f121a341e7ac98
Diego <diedb234@gmail.com>
2022-08-18 13:27:17 +0200

    unicafe 1.6 exercise

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,7 +7,41 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  //Función que actualiza el valor y renderizado del valor good
+  const clickGood = () => {
+    setGood((prevContador) => {
+      return prevContador + 1;
+    });
+  };
+
+  //Función que actualiza el valor y renderizado del valor good
+  const clickNeutral = () => {
+    setNeutral((prevContador) => {
+      return prevContador + 1;
+    });
+  };
+
+  //Función que actualiza el valor y renderizado del valor good
+  const clickBad = () => {
+    setBad((prevContador) => {
+      return prevContador + 1;
+    });
+  };
+
+  //Renderizado de App
+  return (
+    <div className='App'>
+      <h1>Give Feedback</h1>
+      <button onClick={clickGood}>Good</button>
+      <button onClick={clickNeutral}>Neutral</button>
+      <button onClick={clickBad}>Bad</button>
+
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
