7380ecbe73add0b58f806c6eece97c27c7e56114
Diego <diedb234@gmail.com>
2022-08-21 10:21:09 +0200

    Rename Folder

--- Titulo.js
+++
@@ -1,6 +0,0 @@
-const Titulo = (props) => {
-  //Puedes pasar propiedades, pero si las metes como atributo o estilo necesitas doble corchete
-  return <h1 style={{ color: props.color }}>{props.message}</h1>;
-};
-
-export default Titulo;
